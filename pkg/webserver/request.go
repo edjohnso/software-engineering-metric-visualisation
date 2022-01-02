ad5df07cc82c4214fc12b6844840e4f5e119b615
edjohnso <edjohnso@tcd.ie>
2022-01-02 14:36:28 +0000

    Add request.go boilerplate

---
+++ pkg/webserver/request.go
@@ -0,0 +1,7 @@
+package webserver
+
+import "net/http"
+
+func request(token, method, url string) (*http.Response, error) {
+	return nil, nil
+}
---
+++ pkg/webserver/request_test.go
@@ -0,0 +1,13 @@
+package webserver
+
+import "testing"
+
+func TestRequest(t *testing.T) {
+
+	// TODO: test making invalid requests
+	// TODO: test making valid requests
+	// TODO: test making unauthorized requests
+	// TODO: test making authorized requests
+	// TODO: test cached responses
+
+}
