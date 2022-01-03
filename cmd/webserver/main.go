1b9d9b300d64414ab94dac461c7a152f1ea98e09
edjohnso <edjohnso@tcd.ie>
2022-01-03 23:38:49 +0000

    Update main.go to use new CLI args

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <port> <templates>\n", os.Args[0])
+	if len(os.Args) < 5 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <port> <public> <templates> <cache>\n", os.Args[0])
 		os.Exit(1)
 	}
-	if err := webserver.Start(":" + os.Args[1], os.Args[2]); err != nil {
+	if err := webserver.Start(":" + os.Args[1], os.Args[2], os.Args[3], os.Args[4]); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
